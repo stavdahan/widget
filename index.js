274605e469ad6ce2a3fa1dc4834c9c7dbcfc1ea3
Stav Dahan <stav0405@gmail.com>
2023-10-25 20:20:08 +0300

    make fetch prallel

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ import { resizeLastRow, fetchImg } from "./utils.js";
 import { getArticles } from "./api.js";
 import { COUNT } from "./config.js";
 
-const presentedArticles = [];
+let presentedArticles = [];
 
 const createArticlesList = async () => {
   const data = await getArticles();
-  const articles = data.list;
-
-  for (const article of articles) {
-    const res = await fetchImg(article.thumbnail[0].url);
-    if (!!res) {
-      article.src = res;
-      const articleElement = createArticleElement(article);
-      presentedArticles.push(articleElement);
-    }
-    if (presentedArticles.length == COUNT) {
-      break;
-    } 
-  }
-
-  presentedArticles.forEach((presentedArticle) =>
-    document.getElementById("articlesList").appendChild(presentedArticle)
+  const articlesObj = data.list;
+  const images = articlesObj.map((articleObj) =>
+    fetchImg(articleObj.thumbnail[0].url)
   );
+
+  const filteredObjects = await Promise.all(images).then((res) =>
+    res
+      .map((src, idx) => {
+        articlesObj[idx].src = src;
+        return articlesObj[idx];
+      })
+      .filter((article) => !!article.src)
+  );
+
+  presentedArticles = filteredObjects
+    .map((obj) => createArticleElement(obj))
+    .slice(0, COUNT);
+
+  presentedArticles.forEach((article) => {
+    document.getElementById("articlesList").appendChild(article);
+  });
 
   resizeLastRow(window.innerWidth, presentedArticles);
 };
