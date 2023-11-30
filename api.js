58be5fdaaf25cc2d4dc8687f53fda689f8bb8e9f
Stav Dahan <stav0405@gmail.com>
2023-11-30 12:05:32 +0200

    changed db source

--- api.js
+++ api.js
@@ -18,7 +18,8 @@ export const getArticles = async () => {
 };
 
 const createURL = (appType, apiKey, count) => {
-  return `http://api.taboola.com/1.0/json/taboola-templates/recommendations.get?app.type=${appType}&app.apikey=${apiKey}&count=${count}&source.type=video&source.id=214321562187&source.url=http://www.site.com/videos/214321562187.html`;
+  return "/db.json";
+  // return `http://api.taboola.com/1.0/json/taboola-templates/recommendations.get?app.type=${appType}&app.apikey=${apiKey}&count=${count}&source.type=video&source.id=214321562187&source.url=http://www.site.com/videos/214321562187.html`;
 };
 
 const URL = createURL(APPTYPE, APIKEY, GAP);
